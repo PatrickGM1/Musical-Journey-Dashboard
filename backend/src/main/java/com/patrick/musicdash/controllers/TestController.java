879e463a30ca962cabadf4f3876eb40628e67a8f
PatrickGM1 <patrickgheba@gmail.com>
2025-10-13 01:30:40 +0200

    Test backend run (It does nothign so far)

--- backend/src/main/java/com/patrick/musicdash/controllers/TestController.java
+++ backend/src/main/java/com/patrick/musicdash/controllers/TestController.java
@@ -1,10 +1,10 @@
-package com.patrick.musicdash;
+package com.patrick.musicdash.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class TestController {
   @GetMapping("/api/hello")
   public String hello() {
     return "MusicDash backend is up 🎵";
